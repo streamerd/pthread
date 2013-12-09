d3fefc03a0ebf72510ca83b394c742ecbdccbd4b
root <root@zuanyg.(none)>
2013-12-09 13:34:30 +0800

    [modify] add avail conditional varible for result-getting

--- thread_programming_models/pipeline.cpp
+++ thread_programming_models/pipeline.cpp
@@ -2,44 +2,39 @@
 
 #include <cstdio>
 #include <list>
+#include <vector>
 #include <pthread.h>
 
 class processor;
 class pipeline
 {
 private:
-	pthread_mutex_t *mutex;
+	pthread_mutex_t mutex;
 	int active;
 public:
 	std::vector<processor *> line;
 	
-	pipeline(int n): line(n + 1, NULL), mutex(NULL)
+	pipeline(int n): line(n + 2, (processor *)NULL),  
+			 active(0)
 	{
 	}
 
-	~pipeline()
-	{
-		if(mutex) pthread_mutex_destory(mutex);
-		for(int i = 0; i < line.size() - 1; ++i)
-		{
-			if(line[i]) delete line[i];
-		}
-	}	
+	~pipeline();
 	
 	int lock()
 	{
-		return pthread_mutex_lock(mutex);
+		return pthread_mutex_lock(&mutex);
 	}
 	
 	int unlock()
 	{
-		return pthread_mutex_unlock(mutex);
+		return pthread_mutex_unlock(&mutex);
 	}
 	
 	void pipe_start(int send_num);
 	int pipe_result(int &result);
 	int init();	
-}
+};
 
 //processor in the pipeline
 //need to be registed in a pipeline or it will be invalid
@@ -48,12 +43,12 @@ class processor
 private:
 	pthread_t thread;
 	// a mutex connect a data unit and two conditional varibles
-	pthread_mutex_t *mutex;
+	pthread_mutex_t mutex;
 	//ready condition and its predicate
-	pthread_cond_t *ready;
-	//done condition and its predicate, it means the processor can get new data now
-	//but I think In this scene, it doesn't make any sense
-	pthread_cond_t *done;
+	pthread_cond_t ready;
+	//avail condition and its predicate, it means the processor can get new data now
+	//its purpose is to support waiting on the result-getting function
+	pthread_cond_t avail;
 	//the identifier of a processor in the very pipeline
 	int id;
 	pipeline *pipe;
@@ -62,42 +57,50 @@ public:
 	//data
 	int data;
 	bool is_ready;
-	processor(): is_ready(0), id(0), pipe(NULL),
-	             mutex(NULL), ready(NULL), done(NULL),
-		     thread((pthread_t)PTHREAD_MUTEX_INITIALIZER)  
+	processor(): is_ready(0), id(0), pipe(NULL)
 	{
 	}
 	~processor()
 	{
-		if(mutex) pthread_mutex_destroy(mutex);
-		if(ready) pthread_cond_destroy(ready);
-		if(done) pthread_cond_destroy(done);
+		pthread_mutex_destroy(&mutex);
+		pthread_cond_destroy(&ready);
+		pthread_cond_destroy(&avail);
+	}
+	
+	pthread_t & get_thread_instance()
+	{
+		return  thread;	
 	}
 	int init(int identifier, pipeline *boss)
 	{
 		id = identifier;
 		pipe = boss;
-		int statu = pthread_mutex_init(mutex);
+		int statu = pthread_mutex_init(&mutex, NULL);
 		if(statu) return -1;
-		statu = pthread_cond_init(ready);
+		statu = pthread_cond_init(&ready, NULL);
 		if(statu) return -1;
-		statu = pthread_cond_init(done);
+		statu = pthread_cond_init(&avail, NULL);
 		return statu ? -1 : 0;
 	}
 
 	int signal_ready()
 	{
-		return pthread_cond_signal(ready);	
+		return pthread_cond_signal(&ready);	
+	}
+	
+	int signal_avail()
+	{
+		return pthread_cond_signal(&avail);
 	}
 	
 	int lock()
 	{
-		return pthread_mutex_lock(mutex);
+		return pthread_mutex_lock(&mutex);
 	}
 
 	int unlock()
 	{
-		return pthread_mutex_unlock(mutex);
+		return pthread_mutex_unlock(&mutex);
 	}
 
 	//pipeline job you can reload it by different jobs
@@ -109,9 +112,17 @@ public:
 
 	int wait_data_ready()
 	{
-		while(is_ready != 1)
+		while(!is_ready)
 		{
-			pthread_cond_wait(ready, mutex);
+			pthread_cond_wait(&ready, &mutex);
+		}
+	}
+	
+	int wait_data_avail()
+	{
+		while(is_ready)
+		{
+			pthread_cond_wait(&avail, &mutex);
 		}
 	}
 	
@@ -125,31 +136,34 @@ public:
 		while(1)
 		{
 			now->wait_data_ready();
-			if(next) 
-			{
-				next->lock();
-				next->job(now->data);
-				next->signal_ready();
-				next->unlock();
-				is_ready = 0;
-			}
-			else
-			{
-				while(is_ready)
-				{
-					pthread_cond_wait(avail, mutex);
-				}
-			}
+			next->lock();	
+			next->wait_data_avail();
+			next->job(now->data);
+			next->signal_ready();
+			next->unlock();
+			now->is_ready = 0;
+			now->signal_avail();
+			
 		}
 		return NULL;			
 	}
-}
+};
 
+pipeline::~pipeline()
+{
+	pthread_mutex_destroy(&mutex);
+	for(int i = 0; i < line.size(); ++i)
+	{
+		if(line[i]) delete line[i];
+	}
+}	
 void pipeline::pipe_start(int send_num)
 {
 	lock();
 	line[0]->lock();
+	line[0]->wait_data_avail();
 	line[0]->job(send_num);
+	line[0]->signal_ready();
 	line[0]->unlock();
 	++active;
 	unlock();
@@ -162,8 +176,9 @@ int pipeline::pipe_result(int &result)
 	processor *tail = line[line.size() - 2];
 	tail->lock();
 	tail->wait_data_ready();
-	result = tail.data;
+	result = tail->data;
 	tail->is_ready = 0;
+	tail->signal_avail();
 	tail->unlock();	
 	--active;
 	unlock();
@@ -177,9 +192,9 @@ int pipeline::init()
 		line[i] = new processor();
 		if(line[i]->init(i, this)) return -1;
 	}
-	for(int i = 0; i < line.size() - 1; ++i)
+	for(int i = 0; i < line.size() - 2; ++i)
 	{
-		pthread_create(line[i]->thread, NULL, processor::thread_instance, (void *)line[i]);
+		pthread_create(&line[i]->get_thread_instance(), NULL, processor::thread_instance, (void *)line[i]);
 	}
 	return 0;
 }
